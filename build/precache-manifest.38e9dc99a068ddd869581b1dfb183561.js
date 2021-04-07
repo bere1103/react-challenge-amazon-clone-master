a647a2a216ce0378540b6616280356177b4dc094
bere1103 <64001151+bere1103@users.noreply.github.com>
2021-04-08 00:52:34 +0800

    Add files via upload

---
+++ build/precache-manifest.38e9dc99a068ddd869581b1dfb183561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f83f3a9bba745ec0cdecf345b43d651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5df9e01c30f97262b1",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5df9e01c30f97262b1",
+    "url": "/static/js/main.0656627c.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
